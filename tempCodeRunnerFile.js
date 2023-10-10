b19e33c0e6a6bdb42d5710cf3d5803c3b267f9dc
kush <kushc225@gmail.com>
2023-10-10 15:54:45 +0530

    second

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function emp(id, name, salary) {
+  this.id = id;
+  this.name = name;
+  this.salary = salary;
+}
+let a = new emp(1, "ashish", 900);
+console.log(a);
--- test.js
+++
@@ -1,6 +0,0 @@
-let arr = [1, 2, 4];
-
-const obj = arr.reduce((ac, ele, idx) => {
-  return { [idx + 1]: ele, ...ac };
-}, {});
-console.log(obj);
