404be04dc4056ec13787bdf055710a894d9616c5
kush <kushc225@gmail.com>
2023-10-23 15:36:41 +0530

    error in navbar has been resloved

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,10 @@ import UserContext from '@/context/UserContext'
 import React, { useContext } from 'react'
 const Profile = () => {
     const { user } = useContext(UserContext);
-    console.log(user)
+
+    if (user === null) {
+        return <h1>Not user found</h1>
+    }
     return (
         <>
             <div>welcome  {`you name is ${user.name} and your id is ${user.id}, and email is ${user.email}`}</div>
