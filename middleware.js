73630f34d96466884e36e3149d763f725ab6dc24
kush <kushc225@gmail.com>
2023-10-24 11:24:44 +0530

    somecode in middleware added

--- middleware.js
+++ middleware.js
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   // req.cookies.delete("token");
   // console.log("cookies", req.cookies.get("token"));
-  // let token = req.cookies.get("token")?.value || "";
+  let token = req.cookies.get("token")?.value || "";
   // console.log(token);
-  // const currPath = req.nextUrl.pathname;
-  // const safePage = ["/donate", "/need", "/profile"];
+  const currPath = req.nextUrl.pathname;
+  const safePage = ["/donate", "/need", "/profile"];
+  if (safePage.includes(currPath) && token === "") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
   // if (safePage.includes(currPath) && !token) {
   //   const url = req.nextUrl.clone();
   //   url.pathname = "/login";
